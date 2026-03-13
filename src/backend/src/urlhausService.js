90c27435d446843954266b6a6a68fd6e40f960ba
taogenie <taogenie@gmail.com>
2026-03-13 18:14:09 +0800

    fix database for urlhaus sync error

--- src/backend/src/urlhausService.js
+++ src/backend/src/urlhausService.js
@@ -9,6 +9,7 @@ const URLHAUS_FEED = process.env.URLHAUS_URL
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  database: 'omnisense',
   ssl: { rejectUnauthorized: false }
 });
 
